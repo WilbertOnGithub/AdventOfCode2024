d173dd36fd5952f66a37e93d6cbdb9416715d5a7
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2024-12-17 19:56:33 +0100

    WIP: Dag 2

--- AdventOfCode/Dag2/Program.cs
+++ AdventOfCode/Dag2/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Dag2;
+﻿using System.Collections.ObjectModel;
+
+namespace Dag2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        foreach (string line in File.ReadLines("input.txt"))
+        {
+            ReportCollection report = new ReportCollection();
+            foreach (var number in line.Split(' '))
+            {
+                report.Add(int.Parse(number));
+            }
+        }
     }
+
 }
---
+++ AdventOfCode/Dag2/ReportCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Dag2;
+
+public class ReportCollection : Collection<int>
+{
+    public void foo()
+    {
+        var ascendingCopy = new List<int>(Items).OrderBy(x => x);
+        var descendingCopy = new List<int>(Items).OrderByDescending(x => x);
+
+        if (ascendingCopy.SequenceEqual(Items))
+        {
+            Console.WriteLine("Ascending");
+        }
+        else if (descendingCopy.SequenceEqual(Items))
+        {
+            Console.WriteLine("Descending");
+        }
+}
