42a9f892a6036b40fe3c5cf25acbe2c392fe79d6
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2024-12-17 19:19:44 +0100

    Found solution for day 1

--- AdventOfCode/AdventOfCode_Dag1/Program.cs
+++ AdventOfCode/AdventOfCode_Dag1/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace AdventOfCode_Dag1;
+﻿using System.Text.RegularExpressions;
 
-class Program
+namespace AdventOfCode_Dag1;
+
+partial class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<int> firstList = new();
+        List<int> secondList = new();
+
+        foreach (string line in File.ReadLines("input.txt"))
+        {
+            Match match = NumbersRegex().Match(line);
+
+            int first = int.Parse(match.Groups["first"].Value);
+            firstList.Add(first);
+
+            int second = int.Parse(match.Groups["second"].Value);
+            secondList.Add(second);
+        }
+
+        firstList.Sort();
+        secondList.Sort();
+
+        int result = 0;
+        for (int i = 0; i < firstList.Count() ; i++)
+        {
+            result += Math.Abs(firstList[i] - secondList[i]);
+        }
+
+        Console.WriteLine(result);
     }
+
+    [GeneratedRegex(@"^(?<first>\d{5})\s{3}(?<second>\d{5})$")]
+    private static partial Regex NumbersRegex();
 }
